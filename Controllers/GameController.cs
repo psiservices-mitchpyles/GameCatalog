7df3f7ed26e58ca95bfe7691769b913cf1e21a7e
Mitch Pyles <mitchell.pyles@psionline.com>
2023-06-21 16:46:49 -0500

    Controllers for Get Games, GetId, and Post all made. added Models and View Models as well.

---
+++ Controllers/GameController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GameCatalog.Models;
+using GameCatalog.ViewModels;
+using Insight.Database;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+using System.Net.Http.Headers;
+
+namespace GameCatalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public GameController(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("GameCatalog");
+            _dbConnection = new SqlConnection(connectionString);
+        }
+        [HttpGet]
+        public IEnumerable<Game> Get()
+        {
+            return _dbConnection.Query<Game>("GetAllGames");
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Game> Get(int id)
+        {
+            return _dbConnection.Query<Game>("GetId", new { Id = id });
+        }
+        [HttpPost]
+        public void Post([FromBody] AddGame request)
+        {
+            if(String.IsNullOrEmpty(request.Name))
+            {
+            throw new Exception("Invalid Input. Name Cannot be Blank");
+            }
+            if (String.IsNullOrEmpty(request.Developer))
+            {
+                throw new Exception("Invalid Input. Name Cannot be Blank");
+            }
+            var game = new Game
+            {
+            Name = request.Name,
+            Price = request.Price,
+            Developer = request.Developer
+            };  
+            _dbConnection.Execute("AddGame", game);
+            }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace GameCatalog.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Developer { get; set; }
+
+    }
+}
